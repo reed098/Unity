48b36e7926ec1ca737bd1e097d90ec5da40703fb
deniz <64705623+reed098@users.noreply.github.com>
2023-02-26 23:35:31 +0300

    Create CameraTarget.cs

---
+++ CameraTarget.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 target_offset;
+    void Start()
+    {
+        target_offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + target_offset, 0.125f);
+        
+    }
+}
